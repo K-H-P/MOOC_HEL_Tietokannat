dff1c33cb79747cfe09accda90ba0be85687e7f9
krhapa <krhapa@utu.fi>
2021-02-24 18:51:58 +0200

    Functions added, code cleaning, readme update

--- Readme.md
+++ Readme.md
@@ -1,6 +1,13 @@
 # Tietokannat MOOC Helsingin Yliopisto 2021
 
+- [ ] teht1
+- [ ] teht2
+- [ ] teht3
+- [ ] teht4
+- [ ] teht5
+- [ ] teht6
+- [ ] teht7
 
+## Sources
 
-## deb
-https://github.com/xerial/sqlite-jdbc#usage
+- https://github.com/xerial/sqlite-jdbc#usage
--- teht1/kanta.java
+++ teht1/kanta.java
@@ -9,16 +9,65 @@ import java.sql.Statement;
 public class kanta {
 
     public static void main(String[] args) {
-
+        
         Connection conn = null;
         try {
-            conn = DriverManager.getConnection("jdbc:sqlite:./kurssit.db");
+            //Ongelma jdbc kanssa... 
+            conn = DriverManager.getConnection("jdbc:sqlite:kurssit.db");
             Statement statement = conn.createStatement();
             statement.setQueryTimeout(30);
-            System.out.println("Toimii");
+            //System.out.println("Toimii");
+        
+
+            Scanner input = new Scanner(System.in);
+            while (true) {
+                try {
+                    System.out.println("Valitse toiminto 1 - 4: ");
+                    int action = Integer.parseInt(input.nextLine());
+        
+                    if (action == 5) {
+                    System.exit(0);
+                    input.close();
+                    } else if (action == 1) {
+                        function1(conn);
+                    } else if (action == 2) {
+                        function2(conn);
+                    } else if (action == 3) {
+                        function3(conn);
+                    } else if (action == 4) {
+                        function4(conn);
+                    } else {
+                    System.out.println("Kyseistä toimintoa ei ole olemassa.");
+                    }
+  
+                }
+                catch (NumberFormatException e) {
+                    System.out.println("Kyseistä toimintoa ei ole olemassa.");
+                }
+            }
         }
         catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }   
+
+    private static void function1(Connection c) {
+        Scanner one = new Scanner(System.in);
+        System.out.println("Toiminto1");
+    }
+
+    private static void function2(Connection c) {
+        Scanner two = new Scanner(System.in);
+        System.out.println("Toiminto2");
+    }
+
+    private static void function3(Connection c) {
+        Scanner three = new Scanner(System.in);
+        System.out.println("Toiminto3");
+    }
+
+    private static void function4(Connection c) {
+        Scanner four = new Scanner(System.in);
+        System.out.println("Toiminto4");
     }
 }
