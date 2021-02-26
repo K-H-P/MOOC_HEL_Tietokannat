73647e1efb9bc18c5b01cbf056260e6422a8b7ea
krhapa <krhapa@utu.fi>
2021-02-26 02:25:26 +0200

    Modified table creation and added first function

--- teht3/testi.java
+++ teht3/testi.java
@@ -1,58 +1,91 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement; 
-
+import java.sql.*;
+import java.util.Random;
+import java.util.Scanner;
 
 public class testi {
-    
-    private static final String CREATE_TABLE="CREATE TABLE Elokuvat ("
-    + "id INT NOT NULL,"
-    + "nimi VARCHAR(20) NOT NULL,"
-    + "vuosi INT NOT NULL,"
-    + "PRIMARY KEY (id))";
 
+    private static void createTable(Statement statement) {
+        try {
+          statement.execute("CREATE TABLE Elokuvat (id INT PRIMARY KEY, nimi TEXT NOT NULL, vuosi INT);");
+          System.out.println("Luotu Elokuvat taulu");
+        } catch (SQLException e) {
+          System.out.println(e.getMessage());
+        }
+    }
+
+    private static int randYear(int min, int max) {
+        return min + (int)(Math.random() * ((max-min)+1));
+    }
+    
+    private static String randString() {
+        int leftLimit = 97;
+        int rightLimit = 122;
+        int targetStringLength = 15;
+        Random random = new Random();
+    
+        String generatedString = random.ints(leftLimit, rightLimit + 1)
+          .limit(targetStringLength)
+          .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+          .toString();
+        return generatedString;
+    }
+    
     public static void main(String[] args) {
         
         Connection conn = null;
         try {
-            
             conn = DriverManager.getConnection("jdbc:sqlite:testi.db");
             Statement statement = conn.createStatement();
             statement.setQueryTimeout(30);
-            
-            statement.executeUpdate(CREATE_TABLE);
-            System.out.println("Taulu luotu");
 
-
-
-            /*Scanner input = new Scanner(System.in);
+            createTable(statement);
+    
+            Scanner input = new Scanner(System.in);
             while (true) {
-                try {
-                    System.out.println("Valitse toiminto 1 - 3 testejä varten ja 4 lopettaaksesi ohjelman: ");
-                    int action = Integer.parseInt(input.nextLine());
-        
-                    if (action == 4) {
-                    System.exit(0);
-                    input.close();
-                    } else if (action == 1) {
-                        function1(conn);
-                    } else if (action == 2) {
-                        function2(conn);
-                    } else if (action == 3) {
-                        function3(conn);
-                    } else {
-                    System.out.println("Kyseistä toimintoa ei ole olemassa.");
-                    }
-  
+            try {
+                System.out.println("Valitse toiminto 1 - 4 kyselyitä varten ja 5 lopettaaksesi ohjelman: ");
+                int toiminto = Integer.parseInt(input.nextLine());
+    
+                if (toiminto == 5) {
+                System.exit(0);
+                input.close();
+                } else if (toiminto == 1) {
+                    addButtLoadOfRows(conn);;
                 }
-                catch (NumberFormatException e) {
-                    System.out.println("Kyseistä toimintoa ei ole olemassa.");
-                }
-            }*/
-        }
-        catch (SQLException e) {
+            
+            } catch (NumberFormatException e) {
+                System.out.println(e.getMessage());
+            }
+            
+            }
+        } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    private static void addButtLoadOfRows(Connection conn) {
+        try {
+            PreparedStatement start = conn.prepareStatement("BEGIN");
+            PreparedStatement end = conn.prepareStatement("END;");
+            
+            start.executeUpdate();
+            for (int i=0; i<10; i++) {
+              PreparedStatement p = conn.prepareStatement(
+                "INSERT INTO Elokuvat (nimi, vuosi) VALUES(?, ?)"
+              );
+              
+                String randString = randString();
+                int randYear = randYear(1900,2000);
+                p.setString(1, randString);
+                p.setInt(2, randYear);
+                p.executeUpdate();
+            }
+            end.executeUpdate();
+            
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        System.out.println("Lisätty rivit ajassa: ");
     }
 }
